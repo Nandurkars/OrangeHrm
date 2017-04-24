159d8d59e26fcbe73f40d7551094c12ec1bae9c6
nandurkars <snandurkar@lampsplus.com>
2017-04-24 20:16:04 +0530

    test commit

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -1,9 +1,15 @@
 package common;
 
+import java.util.Properties;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import Operation.UIOperation;
 
 public class BaseTest {
 	
@@ -11,7 +17,6 @@ public class BaseTest {
 	
 	@BeforeClass
 	public void launchBrowser(){
-		 //setWebdriverCapabilities();
 		 System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "//lib//chromedriver.exe");
 		 driver = new ChromeDriver();
 		 driver.manage().window().maximize();
@@ -22,5 +27,17 @@ public class BaseTest {
 		driver.quit();
 	}
 	
-	
+	protected void readObectAndPerformOpertions(Properties allObjects, UIOperation operation, Sheet sheet, int rowCount) throws Exception {
+		for (int i = 1; i < rowCount + 1 ; i++) {
+		    Row row = sheet.getRow(i);
+		    if(row.getCell(0).toString().length() == 0){
+		        System.out.println(row.getCell(1).toString() + "----" + row.getCell(2).toString() + "----" + 
+		        row.getCell(3).toString() + "----" + row.getCell(4).toString());
+		        operation.perform(allObjects, row.getCell(1).toString(), row.getCell(2).toString(),
+				row.getCell(3).toString(), row.getCell(4).toString());
+		    }
+		    else
+		            System.out.println("New Testcase->" + row.getCell(0).toString() + " Started");
+		    }
+	}
 }
--- src/test/java/TestClasses/HomePageTest.java
+++ src/test/java/TestClasses/HomePageTest.java
@@ -7,8 +7,6 @@ import Operation.ReadObjectRepo;
 import Operation.UIOperation;
 import common.BaseTest;
 import utilities.ExcelUtils;
-
-import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.testng.annotations.Test;
 
@@ -17,25 +15,14 @@ public class HomePageTest extends BaseTest{
 	@Test
 	public void testLogin() throws Exception {
 		
-	ExcelUtils file = new ExcelUtils();
-	ReadObjectRepo object = new ReadObjectRepo();
-	Properties allObjects = object.getObjectRepository();
-	UIOperation operation = new UIOperation(driver);
-	Sheet sheet = file.readExcel(System.getProperty("user.dir") + "//InputData//HomePageTest.xlsx","HomePageTest.xlsx", "Keyword");
-    int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
-    for (int i = 1; i < rowCount + 1 ; i++) {
-        Row row = sheet.getRow(i);
-        if(row.getCell(0).toString().length() == 0){
-            System.out.println(row.getCell(1).toString() + "----" + row.getCell(2).toString() + "----" + 
-            row.getCell(3).toString() + "----" + row.getCell(4).toString());
-            operation.perform(allObjects, row.getCell(1).toString(), row.getCell(2).toString(),
-    		row.getCell(3).toString(), row.getCell(4).toString());
-        }
-        else{
-                System.out.println("New Testcase->" + row.getCell(0).toString() + " Started");
-            }
-        }
-    Assert.assertEquals("https://enterprise-demo.orangehrmlive.com/dashboad", driver.getCurrentUrl(), "Expected URL doesn't match with the actual URL.");
+		ExcelUtils file = new ExcelUtils();
+		ReadObjectRepo object = new ReadObjectRepo();
+		Properties allObjects = object.getObjectRepository();
+		UIOperation operation = new UIOperation(driver);
+		Sheet sheet = file.readExcel(System.getProperty("user.dir") + "//InputData//HomePageTest.xlsx","HomePageTest.xlsx", "Keyword");
+	    int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+	    readObectAndPerformOpertions(allObjects, operation, sheet, rowCount);
+	    Assert.assertEquals("https://enterprise-demo.orangehrmlive.com/dashboad", driver.getCurrentUrl(), "Expected URL doesn't match with the actual URL.");
     }
 	
 	@Test(dependsOnMethods = { "testLogin" })
