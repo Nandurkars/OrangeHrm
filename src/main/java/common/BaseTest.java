b0704b3f2d98b5ae5d05b1858e4465b30819b9cd
swap <swap@192.168.1.101>
2017-04-26 09:44:19 +0530

    Made excel reader a generic method.

--- src/main/java/common/BaseTest.java
+++ src/main/java/common/BaseTest.java
@@ -27,17 +27,27 @@ public class BaseTest {
 		driver.quit();
 	}
 	
-	protected void readObectAndPerformOpertions(Properties allObjects, UIOperation operation, Sheet sheet, int rowCount) throws Exception {
-		for (int i = 1; i < rowCount+1; i++) {
+	protected void readObectAndPerformOpertions(Properties allObjects, Sheet sheet, String tag, WebDriver driver) throws Exception {
+		UIOperation uIoperation = new UIOperation(driver);
+		boolean isRowStarted = false;
+		tag="signin";
+		int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
+		for (int i = 1; i < rowCount + 1; i++) {
 	        Row row = sheet.getRow(i);
-	        if(row.getCell(0).toString().length()==0){
-	            System.out.println(row.getCell(1).toString()+"----"+ row.getCell(2).toString()+"----"+
-	            row.getCell(3).toString()+"----"+ row.getCell(4).toString());
-	            operation.perform(allObjects, row.getCell(1).toString(), row.getCell(2).toString(),
-	                row.getCell(3).toString(), row.getCell(4).toString());
-	     }
-	        else
-	                System.out.println("New Testcase->"+row.getCell(0).toString() +" Started");
+	       // System.out.println(row.getCell(0));
+	        if((row.getCell(0) !=null && row.getCell(0).toString().trim().equalsIgnoreCase(tag)) || isRowStarted){
+	        	
+	        	if(row.getCell(0) != null && !row.getCell(0).toString().trim().isEmpty() && isRowStarted){
+	        		break;
+	        	}
+	        	isRowStarted = true;
+	        	System.out.println(row.getCell(1).toString() + "*****" + row.getCell(2).toString() + "*****" + row.getCell(3).toString() + "*****" + row.getCell(4).toString());
+	        	String operation = row.getCell(1).toString();
+	        	String objectName = row.getCell(2).toString();
+	        	String objectType = row.getCell(3).toString();
+	        	String objectValue = row.getCell(4).toString();
+	        	//uIoperation.perform(allObjects, operation, objectName, objectType, objectValue);
+	        }
 	    }
 	}
 }
--- src/test/java/TestClasses/HomePageTest.java
+++ src/test/java/TestClasses/HomePageTest.java
@@ -3,7 +3,6 @@ package TestClasses;
 import java.util.Properties;
 
 import Operation.ReadObjectRepo;
-import Operation.UIOperation;
 import common.BaseTest;
 import utilities.ExcelUtils;
 import org.apache.poi.ss.usermodel.Sheet;
@@ -12,16 +11,15 @@ import org.testng.annotations.Test;
 
 public class HomePageTest extends BaseTest{
 
-	@Test(priority = 0)
+	@Test
 	public void testLogin() throws Exception {
 		
 		ExcelUtils file = new ExcelUtils();
 		ReadObjectRepo object = new ReadObjectRepo();
 		Properties allObjects = object.getObjectRepository();
-		UIOperation operation = new UIOperation(driver);
 		Sheet sheet = file.readExcel(System.getProperty("user.dir") + "//InputData//HomePageTest.xlsx","HomePageTest.xlsx", "Keyword");
-	    int rowCount = sheet.getLastRowNum() - sheet.getFirstRowNum();
-	    readObectAndPerformOpertions(allObjects, operation, sheet, rowCount);
+	    String tag = "login";
+	    readObectAndPerformOpertions(allObjects, sheet, tag, driver);
 	    Thread.sleep(5000);
 	    Assert.assertEquals("http://opensource.demo.orangehrmlive.com/index.php/dashboard", driver.getCurrentUrl(), "Expected URL doesn't match with the actual URL.");
     }
